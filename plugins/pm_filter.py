5579e4842f723eea951386355fa1d17979dfa036
TEAM DARK DEVIL <111672563+DARK-DEVIL-BOTZ@users.noreply.github.com>
2022-09-09 19:47:15 +0530

    💡DARK-DEVIL-BOTZ™️🇱🇰

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -364,7 +364,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     caption=f_caption,
                     protect_content=True if ident == "filep" else False 
                 )
-                await query.answer('Check PM, Film Detective sent files in PM', show_alert=True)
+                await query.answer('Check your privat messeges. I will sent to you...', show_alert=True)
         except UserIsBlocked:
             await query.answer('Unblock me bro !', show_alert=True)
         except PeerIdInvalid:
